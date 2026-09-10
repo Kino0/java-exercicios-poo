0aeb1b689bc107dad244aab27f7b48c193ba29a1
Denis <contato.denismauricio@gmailcom>
2026-09-10 05:06:18 -0300

    feat: exclui anotacao inicial da ide

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,6 @@ import autonomia.Veiculo;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Veiculo carro1 = new Carro("Honda","NSX");
