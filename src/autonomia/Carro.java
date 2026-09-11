ad42417971eb17c6b7904abc3ed46751bf8ed74f
Denis <contato.denismauricio@gmailcom>
2026-09-11 01:19:01 -0300

    fix: corrige logica de metodos abstratos implementados

--- src/autonomia/Carro.java
+++ src/autonomia/Carro.java
@@ -7,20 +7,14 @@ public class Carro extends Veiculo {
     }
 
     @Override
-    public double calcularAutonomia(double litros){
-        double kilometragem = litros * getKmLitro();
-        System.out.print("O veículo " + getModelo() +
-                " tem autonomia de " + kilometragem +
-                " km com " + litros + " litros.\n");
-        System.out.println("-".repeat(7));
-        return kilometragem;
+    public double getKmLitro() {
+        return 12.0;
     }
 
     @Override
-    public double getKmLitro(){ return 12.0; }
-    public double calcularTaxaManutencao(){
+    public double calcularTaxaManutencao() {
         double taxa = 500.0;
-        System.out.println("Taxa de serviço: R$" + taxa);
-        return 500.0;
+        System.out.println("Taxa de manutenção do Carro (" + getModelo() + "): R$" + taxa);
+        return taxa;
     }
 }
